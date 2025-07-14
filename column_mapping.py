080702b66558c3df7e76e72554d7131384cea9e7
Mistry <Dhiraj.Mistry@jll.com>
2025-07-14 12:07:18 +0530

    changed to gpt model

--- column_mapping.py
+++ column_mapping.py
@@ -16,7 +16,7 @@ logger = setup_logger(__name__)
 
 base_url = "https://api-prod.jll.com/csp/chat-service/api/v1"
 subscription_key = "e3c3d48a04e04f10be62efffd0f972ac"
-model = "GEMINI_15_PRO"
+model = "GPT_4_O"
 
 # Okta Configuration
 OKTA_TOKEN_URL="https://api-prod.jll.com/okta/token"
